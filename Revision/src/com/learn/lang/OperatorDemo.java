43c7c4d9ec5754d6d5358517f2144c8013eb58fd
Sanjeev Kumar Gupta <sanjeev.kumar.gupta@ericsson.com>
2022-08-28 17:52:47 +0530

    first commit

---
+++ Revision/src/com/learn/lang/OperatorDemo.java
@@ -0,0 +1,15 @@
+package com.learn.lang;
+
+public class OperatorDemo {
+
+	public static void main(String[] args) {
+	  int a = 10, b=20;
+	  boolean res =a>b;
+	  System.out.println("res");
+	  
+	  int c = (a>b)?a:b;
+	  System.out.println(c);
+
+	}
+
+}
